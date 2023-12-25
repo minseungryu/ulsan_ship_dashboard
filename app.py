e2b41ebb7f9ac839883b5f6f63203c60d9c4656d
류민승 <ryuminseung96@gmail.com>
2023-12-25 13:37:44 +0900

    trial2

--- app.py
+++ app.py
@@ -11,22 +11,25 @@ from pages.pred2 import predict_ship_waiting_time
 from pages.home import about_service
 from pages.eda import ulsan_eda
 from pages.data import show_data
-
 import warnings
 warnings.filterwarnings('ignore')
 
-with st.sidebar:
-    selected_menu = option_menu('Welcome', ["About Service", 'EDA', "Prediction", "Data"],
-                           icons = ['search', 'clipboard-data', 'magic', 'database'], menu_icon="Geo", default_index=0) #icon : https://icons.getbootstrap.com/
+def main():
+    with st.sidebar:
+        selected_menu = option_menu('Welcome', ["About Service", 'EDA', "Prediction", "Data"],
+                            icons = ['search', 'clipboard-data', 'magic', 'database'], menu_icon="Geo", default_index=0) #icon : https://icons.getbootstrap.com/
 
-if selected_menu == "About Service":
-    about_service()
+    if selected_menu == "About Service":
+        about_service()
 
-if selected_menu == "Prediction":
-    predict_ship_waiting_time()  # pred.py의 함수 호출
+    if selected_menu == "Prediction":
+        predict_ship_waiting_time()  # pred.py의 함수 호출
 
-if selected_menu == 'EDA':
-    ulsan_eda()
+    if selected_menu == 'EDA':
+        ulsan_eda()
 
-elif selected_menu == "Data":
-    show_data()
+    elif selected_menu == "Data":
+        show_data()
+
+if __name__ == "__main__":
+    main()
